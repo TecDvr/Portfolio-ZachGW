d5dc074e4f4c4cbf96f03a4db209d7f9e32f3965
Zach Gwirtz <zachgw@gmail.com>
2020-01-06 15:01:30 -0800

    view height update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let vh = window.innerHeight * 0.01
-document.documentElement.style.setProperty('--vh', `${vh}px`)
+document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
 
 function hamburgerOpen() {
     let element = document.getElementById("myDIV");
