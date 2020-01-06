37ee90119f270994fa234ce08cd328ebbc0747c4
Zach Gwirtz <zachgw@gmail.com>
2020-01-06 15:51:16 -0800

    resize fix

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+function setDocHeight() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`) 
+};
+
+window.addEventListener('resize', function () {
+    setDocHeight();
+});
+window.addEventListener('orientationchange', function () {
+    setDocHeight();
+});
+    
+setDocHeight();
 
 function hamburgerOpen() {
     let element = document.getElementById("myDIV");
